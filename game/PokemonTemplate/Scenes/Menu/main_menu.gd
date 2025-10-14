e41094f661fb4c46394a97c165478ecdb3028b50
AditFadhillah <fadhillah.adit@gmail.com>
2025-10-14 06:26:30 +0200

    Revert to previous Godot version

--- game/PokemonTemplate/Scenes/Menu/main_menu.gd
+++ game/PokemonTemplate/Scenes/Menu/main_menu.gd
@@ -1,12 +1,7 @@
 extends Control
 
-@onready var player_name_label = $VBoxContainer/PlayerNameLabel
-
 func _ready():
 	$Music.play()
-	
-	# Display default player info
-	player_name_label.text = "Welcome, Player"
 
 func _process(delta):
 	if Input.is_action_pressed("start"):
@@ -14,7 +9,3 @@ func _process(delta):
 
 func load_game():
 	get_tree().change_scene_to_file("res://Scenes/Levels/world.tscn")
-
-func _on_stats_button_pressed():
-	# Show player stats scene
-	get_tree().change_scene_to_file("res://Scenes/Menu/player_stats.tscn")
